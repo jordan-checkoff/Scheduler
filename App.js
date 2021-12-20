f1c079b3602a8a6f30a90adf026310ce2a7a2f19
Jordan Checkoff <jordan.checkoff@gmail.com>
2021-12-19 22:35:00 -0500

    Fetch Data from URL

--- App.js
+++ App.js
@@ -1,35 +1,49 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import CourseList from './components/CourseList';
 
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
+// const schedule = {
+//   title: "CS Courses for 2018-2019",
+//   courses: [
+//     {
+//       "id": "F101",
+//       "title": "Computer Science: Concepts, Philosophy, and Connections",
+//       "meets": "MWF 11:00-11:50"
+//     },
+//     {
+//       "id": "F110",
+//       "title": "Intro Programming for non-majors",
+//       "meets": "MWF 10:00-10:50"
+//     },
+//     {
+//       "id": "F111",
+//       "title": "Fundamentals of Computer Programming I",
+//       "meets": "MWF 13:00-13:50"
+//     },
+//     {
+//       "id": "F211",
+//       "title": "Fundamentals of Computer Programming II",
+//       "meets": "TuTh 12:30-13:50"
+//     }
+//   ]
+// };
 
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
@@ -39,8 +53,15 @@ const App = () => {
 }
 
 const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
+
+const fetchSchedule = async () => {
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  const json = await response.json();
+  setSchedule(json);
+};
 
 const styles = StyleSheet.create({
   container: {
